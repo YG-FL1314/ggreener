b2e34086190fa3bad1536817fc33fa1f0060b1ab
YG-FL1314 <953559491@qq.com>
2020-04-12 16:23:13 +0800

    bugfix

--- src/main/java/com/ggreener/oa/util/TransformUtil.java
+++ src/main/java/com/ggreener/oa/util/TransformUtil.java
@@ -6,6 +6,7 @@ import com.alibaba.fastjson.TypeReference;
 import com.ggreener.oa.po.CompanyPO;
 import com.ggreener.oa.po.CompanyTagsPO;
 import com.ggreener.oa.vo.CompanyVO;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,6 +26,9 @@ public class TransformUtil {
             throw new Exception("公司名称为空！");
         }
         CompanyPO company = JSON.parseObject(json.toString(), new TypeReference<CompanyPO>(){});
+        if (StringUtils.isNotEmpty(company.getBrief()) && company.getBrief().length() > 1000) {
+            throw new Exception("公司简介不能超过1000个字符");
+        }
         company.setStatus(Constants.STATUS_NORMAL);
         company.setCreateTime(new Date());
         company.setCreateUser(uuid);
